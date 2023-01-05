96ea9c3cd80161daa8fdc8b1f376f5d3a41b0281
katepryshchepiuk <katepryshchepiuk@users.noreply.github.com>
2023-01-05 21:37:53 +0100

    Added Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -64,11 +64,16 @@ const Header = () => {
           alignItems="center"
         >
           <nav>
-            {/* Add social media links based on the `socials` data */}
+            <HStack spacing={4}>
+              {socials.map((social) => (
+                <a href={social.url}><FontAwesomeIcon icon={social.icon} size="1x" /></a>
+              ))}
+            </HStack>
           </nav>
           <nav>
             <HStack spacing={8}>
-              {/* Add links to Projects and Contact me section */}
+              <a onClick={handleClick("projects")}>Projects</a>
+              <a onClick={handleClick("contactme")}>Contact Me</a>
             </HStack>
           </nav>
         </HStack>
